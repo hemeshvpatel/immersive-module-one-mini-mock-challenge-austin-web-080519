300e54687413dfe17f14bc352403b17d7373a72f
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-08-12 10:35:23 -0500

    Done.

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+class Author
+
+attr_accessor :name
+
+@@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select{|book| book.author == self}
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        self.books.reduce(0) {|sum,book| sum + book.word_count}
+    end
+
+    def self.most_words
+       #self.all.max {|a,b | a.total_words <==> b.total_words}
+        author_name_sorted = Author.all.sort_by {|author|author.total_words}
+        author_wc_sorted = Author.all.map {|author| author.total_words}
+        result = "#{author_name_sorted.last} #{author_wc_sorted.last}"
+    end
+
+end
+
 
--- book.rb
+++ book.rb
@@ -1,1 +1,19 @@
+class Book
 
+    attr_accessor :author, :title
+    attr_reader :word_count
+
+    @@all = []
+
+    def initialize(title, author, word_count)
+        @title = title
+        @author = author
+        @word_count = word_count
+        @@self << self
+    end
+
+    self.all
+        @@all
+    end
+
+end
